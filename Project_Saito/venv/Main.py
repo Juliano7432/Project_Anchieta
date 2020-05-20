6135a79dbf7531d1c9d26bf5faf9222e0fa1f348
Jbaldui <61895428+Jbaldui@users.noreply.github.com>
2020-05-19 23:27:59 -0300

    Initial commit

---
+++ Project_Saito/venv/Main.py
@@ -0,0 +1,33 @@
+"""
+Start Data - 19/05/2020
+Project Name - Pizzaria de la puta que lo pario
+File - Main.py
+Authors - Jefferson Balduino // Victor Machado // Henrique Mariano
+
+Changes Log - 05/19/2020 - R01 - File Created
+"""
+
+import os
+import sqlite3
+
+#define DB client
+fileDB =
+
+"""
+def main():
+    menuPrincipal()
+
+
+def menuPrincipal():
+    print('Main Menu')
+    print('1 - Add New Item')
+    print('2 - Edit Existing Item')
+    print('3 - Help')
+    print('Type your option: ')
+    selection = int(input())
+
+
+if __name__ == '__main__':
+    main()
+
+"""
